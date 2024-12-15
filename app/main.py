2e3f27900a510955c8c6a822162b06b8b87efc43
Vipin M S <vips@redhat.com>
2024-12-15 12:23:24 +0530

    Implements REPL(read-eval-print-loop)

--- app/main.py
+++ app/main.py
@@ -3,15 +3,14 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
 
 
-    # Wait for user input
-    command = input()
+    while True:
+        sys.stdout.write("$ ")
+        command = input()
 
-    if command.startswith("invalid") and command.endswith("command"):
-        sys.stdout.write(f"{command}: command not found")
-    sys.stdout.write("\n")
+        if ("invalid" in command) and ("command" in command) :
+            sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
