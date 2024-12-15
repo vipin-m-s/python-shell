4a934c1ce09bf8d37eab2748332a75607c97819c
Vipin M S <vips@redhat.com>
2024-12-15 12:42:54 +0530

    Implements type command

--- app/main.py
+++ app/main.py
@@ -4,22 +4,30 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
 
-    valid_commands = {"exit 0"}
+    builtin_commands = {"exit", "echo", "type"}
 
     while True:
         sys.stdout.write("$ ")
         command = input()
 
-        
-        
         if command == "exit 0":
             sys.exit(0) 
+        
         elif command.startswith("echo") :
             sys.stdout.write(command.split("echo ")[1])
+        
+        elif command.startswith("type"):
+            command_to_check = command.split("type ")[1]
+
+            if command_to_check in builtin_commands:
+                sys.stdout.write(f"{command_to_check} is a shell builtin")
+            else:
+                sys.stdout.write(f"{command_to_check}: not found")
+    
         else: 
             sys.stdout.write(f"{command}: command not found")
 
         sys.stdout.write("\n")
-        
+
 if __name__ == "__main__":
     main()
