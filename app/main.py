c744c5bbe12f459af0bc30d85eff599a7bc9c9c4
Vipin M S <vips@redhat.com>
2024-12-15 12:36:54 +0530

    Adds support for echo

--- app/main.py
+++ app/main.py
@@ -10,11 +10,16 @@ def main():
         sys.stdout.write("$ ")
         command = input()
 
-        if command not in valid_commands :
-            sys.stdout.write(f"{command}: command not found\n")
+        
         
         if command == "exit 0":
             sys.exit(0) 
+        elif command.startswith("echo") :
+            sys.stdout.write(command.split("echo ")[1])
+        else: 
+            sys.stdout.write(f"{command}: command not found")
 
+        sys.stdout.write("\n")
+        
 if __name__ == "__main__":
     main()
